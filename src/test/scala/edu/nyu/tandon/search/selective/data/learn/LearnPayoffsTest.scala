80917bf9c68a25da11743811291b97400a42048d
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2017-02-01 16:56:40 -0500

    dataFrameFromBasename removed

--- src/test/scala/edu/nyu/tandon/search/selective/data/learn/LearnPayoffsTest.scala
+++ src/test/scala/edu/nyu/tandon/search/selective/data/learn/LearnPayoffsTest.scala
@@ -11,32 +11,32 @@ import org.scalatest.Matchers._
   */
 class LearnPayoffsTest extends BaseFunSuite {
 
-  test("dataFrameFromBasename") {
-    val actual = LearnPayoffs.trainingDataFromBasename(s"$resourcesPath/test").collectAsList()
-    val expected = List(
-      Row(Vectors.dense(2.0, 0.1, 0.001, 0.0), 1.0),
-      Row(Vectors.dense(2.0, 0.1, 0.001, 1.0), 0.0),
-      Row(Vectors.dense(2.0, 0.1, 0.001, 2.0), 0.0),
-      Row(Vectors.dense(2.0, 0.5, 0.005, 0.0), 3.0),
-      Row(Vectors.dense(2.0, 0.5, 0.005, 1.0), 3.0),
-      Row(Vectors.dense(2.0, 0.5, 0.005, 2.0), 0.0),
-
-      Row(Vectors.dense(2.0, 0.1, 0.001, 0.0), 3.0),
-      Row(Vectors.dense(2.0, 0.1, 0.001, 1.0), 2.0),
-      Row(Vectors.dense(2.0, 0.1, 0.001, 2.0), 1.0),
-      Row(Vectors.dense(2.0, 0.5, 0.005, 0.0), 0.0),
-      Row(Vectors.dense(2.0, 0.5, 0.005, 1.0), 0.0),
-      Row(Vectors.dense(2.0, 0.5, 0.005, 2.0), 0.0),
-
-      Row(Vectors.dense(2.0, 0.1, 0.001, 0.0), 2.0),
-      Row(Vectors.dense(2.0, 0.1, 0.001, 1.0), 1.0),
-      Row(Vectors.dense(2.0, 0.1, 0.001, 2.0), 0.0),
-      Row(Vectors.dense(2.0, 0.5, 0.005, 0.0), 0.0),
-      Row(Vectors.dense(2.0, 0.5, 0.005, 1.0), 0.0),
-      Row(Vectors.dense(2.0, 0.5, 0.005, 2.0), 0.0)
-    )
-
-    actual should contain theSameElementsAs expected
-  }
+//  test("dataFrameFromBasename") {
+//    val actual = LearnPayoffs.trainingDataFromBasename(s"$resourcesPath/test").collectAsList()
+//    val expected = List(
+//      Row(Vectors.dense(2.0, 0.1, 0.001, 0.0), 1.0),
+//      Row(Vectors.dense(2.0, 0.1, 0.001, 1.0), 0.0),
+//      Row(Vectors.dense(2.0, 0.1, 0.001, 2.0), 0.0),
+//      Row(Vectors.dense(2.0, 0.5, 0.005, 0.0), 3.0),
+//      Row(Vectors.dense(2.0, 0.5, 0.005, 1.0), 3.0),
+//      Row(Vectors.dense(2.0, 0.5, 0.005, 2.0), 0.0),
+//
+//      Row(Vectors.dense(2.0, 0.1, 0.001, 0.0), 3.0),
+//      Row(Vectors.dense(2.0, 0.1, 0.001, 1.0), 2.0),
+//      Row(Vectors.dense(2.0, 0.1, 0.001, 2.0), 1.0),
+//      Row(Vectors.dense(2.0, 0.5, 0.005, 0.0), 0.0),
+//      Row(Vectors.dense(2.0, 0.5, 0.005, 1.0), 0.0),
+//      Row(Vectors.dense(2.0, 0.5, 0.005, 2.0), 0.0),
+//
+//      Row(Vectors.dense(2.0, 0.1, 0.001, 0.0), 2.0),
+//      Row(Vectors.dense(2.0, 0.1, 0.001, 1.0), 1.0),
+//      Row(Vectors.dense(2.0, 0.1, 0.001, 2.0), 0.0),
+//      Row(Vectors.dense(2.0, 0.5, 0.005, 0.0), 0.0),
+//      Row(Vectors.dense(2.0, 0.5, 0.005, 1.0), 0.0),
+//      Row(Vectors.dense(2.0, 0.5, 0.005, 2.0), 0.0)
+//    )
+//
+//    actual should contain theSameElementsAs expected
+//  }
 
 }
