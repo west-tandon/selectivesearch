bc5d57902687c5763dd0209f5b4ceead813e147f
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2017-03-02 12:54:32 -0500

    Complex rank function converter

--- scripts/complex.py
+++ scripts/complex.py
@@ -5,6 +5,7 @@ import numpy as np
 
 parser = argparse.ArgumentParser(description='Convert a complex-ranking-function result file to a format supported by selectivesearch', prog='complex')
 parser.add_argument('input', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+parser.add_argument('output')
 parser.add_argument('--map', '-m', type=argparse.FileType('r'))
 parser.add_argument('--count', '-c', type=int)
 args = parser.parse_args()
@@ -27,5 +28,13 @@ def m(id):
 
 data = pd.read_csv(args.input, sep=' ')
 data.columns = ['qid', 'did', 'score', 'cscore']
-for qid, group in data.groupby('qid'):
-    print(' '.join([str(m(did)) for did in group.sort_values(by='cscore', ascending=False)['did']]))
+
+with open('{}.results.global'.format(args.output), 'w') as results:
+    with open('{}.scores'.format(args.output), 'w') as scores:
+        for qid, group in data.groupby('qid'):
+            sorted = [(int(row[0]), row[1]) for row in group.sort_values(by='cscore', ascending=False)[['did', 'cscore']].values]
+            # print(sorted)
+            results.write(' '.join([str(m(did)) for did, s in sorted]))
+            results.write('\n')
+            scores.write(' '.join([str(s) for did, s in sorted]))
+            scores.write('\n')
