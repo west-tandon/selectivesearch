ecc6e03a87ec64643d92a93724993e4ea41f54db
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2017-05-07 21:41:20 -0400

    script: duplicate buckets for impacts

--- scripts/duplicate-buckets.py
+++ scripts/duplicate-buckets.py
@@ -6,14 +6,14 @@ from fastparquet import ParquetFile, write
 parser = argparse.ArgumentParser(description='Duplicate bucket 0 n times, producing n buckets', prog='duplicate-buckets')
 parser.add_argument('input_prefix')
 parser.add_argument('output_prefix')
-parser.add_argument('num-shards', type=int)
-parser.add_argument('num-buckets', type=int)
+parser.add_argument('shards', type=int)
+parser.add_argument('buckets', type=int)
 args = parser.parse_args()
 
 
-for shard in range(args.num_shards):
-    input_df = ParquetFile("{}#{}.impacts".format(args.input, shard)).to_pandas()
-    output_dfs = [input_df.copy(deep=True) for bucket in range(args.num_buckets)]
+for shard in range(args.shards):
+    input_df = ParquetFile("{}#{}.impacts".format(args.input_prefix, shard)).to_pandas()
+    output_dfs = [input_df.copy(deep=True) for bucket in range(args.buckets)]
     for bucket, df in enumerate(output_dfs):
         df['bucket'] = bucket
         df['bucket'] = df['bucket'].astype(np.int32)
